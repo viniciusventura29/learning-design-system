330a0652bb04ff36bb298629de0202ea49468839
viniciusventura29 <viniciusventura29@gmail.com>
2022-10-18 11:15:13 -0300

    Improving switch component

--- src/Components/Switch.stories.tsx
+++ src/Components/Switch.stories.tsx
@@ -5,20 +5,76 @@ import { Text } from "./Text";
 export default {
   title: "Components/Switch",
   component: Switch,
-  args: {
-    children: "Button Text",
+  argTypes: {
+    size: {
+      options: ["sm", "md", "lg"],
+      control: {
+        type: "inline-radio",
+      },
+    },
   },
+
 
   decorators:[
     (Story) => {
         return(
             <div className="flex items-center gap-2">
                 {Story()}
-                <Text size='sm'>Your text here</Text>
             </div>
         )
     }
   ]
 } as Meta<SwitchProps>;
 
-export const Default: StoryObj<SwitchProps> = {};
+export const Default: StoryObj<SwitchProps> = {
+  argTypes:{
+    asChild:{
+      table:{
+        disable:true
+      }
+    },
+    className:{
+      table:{
+        disable:true
+      }
+    }
+  }
+};
+
+export const Small: StoryObj<SwitchProps> = {
+  args:{
+    size:'sm'
+  },
+  argTypes:{
+    asChild:{
+      table:{
+        disable:true
+      }
+    },
+    className:{
+      table:{
+        disable:true
+      }
+    }
+  }
+};
+
+
+export const Large: StoryObj<SwitchProps> = {
+  args:{
+    size:'lg'
+  },
+  argTypes:{
+    asChild:{
+      table:{
+        disable:true
+      }
+    },
+    className:{
+      table:{
+        disable:true
+      }
+    }
+  }
+};
+
--- src/Components/Switch.tsx
+++ src/Components/Switch.tsx
@@ -4,18 +4,29 @@ import React from "react";
 
 export interface SwitchProps extends SwitchPrimitive.SwitchProps {
   className?: string
+  size?: 'sm' | 'md' | 'lg'
 }
 
-export function Switch ({className}: SwitchProps) {
+export function Switch ({className,size='md'}: SwitchProps) {
   return (
     <SwitchPrimitive.Root
       className={clsx(
-        "group radix-state-checked:bg-cyan-500 radix-state-unchecked:bg-gray-200 relative inline-flex h-[24px] w-[44px] flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out",
+        "group radix-state-checked:bg-cyan-500 radix-state-unchecked:bg-gray-200 relative inline-flex flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out",
+        {
+          'w-[40px] h-[20px]': size === 'sm',
+          'w-[45px] h-[25px]': size === 'md',
+          'w-[50px] h-[30px]': size === 'lg'
+        },
         className)}
     >
       <SwitchPrimitive.Thumb
         className={clsx(
-          "group-radix-state-checked:translate-x-5 group-radix-state-unchecked:translate-x-0 pointer-events-none inline-block h-[20px] w-[20px] transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out"
+          "group-radix-state-checked:translate-x-5 group-radix-state-unchecked:translate-x-0 pointer-events-none inline-block transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out",
+          {
+            'w-[16px] h-[16px]': size === 'sm',
+            'w-[21px] h-[21px]': size === 'md',
+            'w-[26px] h-[26px]': size === 'lg'
+          }
         )}
       />
     </SwitchPrimitive.Root>
